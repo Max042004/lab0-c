94f20f70d9b9cddb28ec66e14907acd0a94692e7
Max042004 <sau525@gmail.com>
2025-03-01 20:35:08 +0800

    Implement q_ascend, q_descend
    q_ascend/q_descend will remove every element which has
    a element with a strictly less/greater value anywhere
    to the right side of it.
    
    Change-Id: I316dad65569e7f795216b2c22037682913d298f6

--- queue.c
+++ queue.c
@@ -262,16 +262,44 @@ void q_sort(struct list_head *head, bool descend)
  * the right side of it */
 int q_ascend(struct list_head *head)
 {
-    // https://leetcode.com/problems/remove-nodes-from-linked-list/
-    return 0;
+    if (!head || list_empty(head))
+        return 0;
+
+    struct list_head *curr = head->prev;
+    while (curr->prev != head) {
+        if (strcmp(list_entry(curr, element_t, list)->value,
+                   list_entry(curr->prev, element_t, list)->value) < 0) {
+            struct list_head *tmp = curr->prev;
+            list_del(tmp);
+            q_release_element(list_entry(tmp, element_t, list));
+        } else {
+            curr = curr->prev;
+        }
+    }
+
+    return q_size(head);
 }
 
 /* Remove every node which has a node with a strictly greater value anywhere to
  * the right side of it */
 int q_descend(struct list_head *head)
 {
-    // https://leetcode.com/problems/remove-nodes-from-linked-list/
-    return 0;
+    if (!head || list_empty(head))
+        return 0;
+
+    struct list_head *curr = head->prev;
+    while (curr->prev != head) {
+        if (strcmp(list_entry(curr, element_t, list)->value,
+                   list_entry(curr->prev, element_t, list)->value) > 0) {
+            struct list_head *tmp = curr->prev;
+            list_del(tmp);
+            q_release_element(list_entry(tmp, element_t, list));
+        } else {
+            curr = curr->prev;
+        }
+    }
+
+    return q_size(head);
 }
 
 /* Merge all the queues into one sorted queue, which is in ascending/descending
